5ba47baaeac344c0e8d43987d21bf18529504168
Max Yeo <mzwyeo@gmail.com>
2016-03-13 20:01:08 -0400

    designed and finished static archive page

---
+++ app/archive.php
@@ -0,0 +1,104 @@
+<?php
+// CONNECT TO THE DATABASE
+// CONFIG.PHP IS HIDDEN FROM GIT REPO FOR SECURITY
+
+// require 'config.php';
+// try {
+//     $conn = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+// }
+// catch(PDOException $e) {
+//     header('location: sorry.html');
+// }
+?>
+
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+
+    <title>Tessa</title>
+    <meta name="description" content="">
+    <meta name="author" content="Alwin Hui and Max Yeo">
+
+    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+
+    <meta name="theme-color" content="">
+
+    <meta property="og:title" content=""/>
+    <meta property="og:url" content=""/>
+    <meta property="og:image" content=""/>
+    <meta property="og:site_name" content="Tessa"/>
+    <meta property="og:description" content=""/>
+
+    <link rel="stylesheet" type="text/css" href="global.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+    <div id="archive">
+        <a href="index.php" id="logo"><div></div></a>
+        <footer>
+            <a href="submit.php" id="submit-link">
+                <h6>SUBMIT</h6>
+                <i class="fa fa-pencil"></i>
+            </a>
+            <a href="index.php" id="archive-link">
+                <h6>ARCHIVES</h6>
+                <i class="fa fa-times"></i>
+            </a>
+        </footer>
+        
+        <a href="index.php" class="col-1-3 message" style="background-color: #92c3ec;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #75e2ec;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #f3a4c8;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #f2c28d;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #a563ef;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #47c2f1;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #79eab0;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #92c3ec;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+        <a href="index.php" class="col-1-3 message" style="background-color: #75e2ec;">
+            <div class="message-border"><div></div></div>
+            <h1 style="font-size: 28px;">Get Well Soon! You're the kindest and most beautiful person I know!</h1>
+            <h3>- Alwin Hui</h3>
+        </a>
+
+    </div>
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/style.js"></scsript>
+</body>
+</html>
